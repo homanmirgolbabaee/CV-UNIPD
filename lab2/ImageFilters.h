0452d8c2a46644e6c40f61058ac0d9e46b2341e2
Homan Mirgolbabaee <homanmirgolbabaee@gmail.com>
2024-03-22 21:36:17 +0100

    Update ImageFilters.h

--- lab2/ImageFilters.h
+++ lab2/ImageFilters.h
@@ -4,11 +4,16 @@
 #include <opencv2/opencv.hpp>
 #include <string>
 
-// Declare the filtering functions
+// Existing declarations
 cv::Mat maxFilter(const cv::Mat& src, int kernelSize);
 cv::Mat minFilter(const cv::Mat& src, int kernelSize);
+void showResizedWindow(const std::string& winname, const cv::Mat& img, int width = 640, int height = 480);
+// New declarations for Tasks 3, 4, 5, and 6
+cv::Mat medianFilter(const cv::Mat& src, int kernelSize);
+// In ImageFilters.h
+cv::Mat gaussianSmoothing(const cv::Mat& src, int kernelSize, double sigmaX, double sigmaY = 0);
 
-// Declare the utility function for showing images in a resized window
-void showResizedWindow(const std::string& winname, const cv::Mat& img, int width = 200, int height = 200);
+void plotHistogram(const cv::Mat& src);
+cv::Mat histogramEqualization(const cv::Mat& src);
 
 #endif // IMAGEFILTERS_H
